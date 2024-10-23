f5f9d0e514e91eca90ccd50bc568902cf8e95f8f
Inema_Leslie <a.inema2@alustudent.com>
2024-10-23 03:31:58 -0700

    n2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def add(a, b):
-    return a + b
+    result = a + b
+    print("The sum of {} and {} is {}".format(a, b, result))  # Adding .format() to print the sum
+    return result
