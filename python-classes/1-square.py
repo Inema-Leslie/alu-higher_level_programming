200978107fee6c9f01fd2da20ccb65e868794fa7
Inema_Leslie <a.inema2@alustudent.com>
2024-11-01 05:53:35 -0700

    n1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-"""Define a square """
 
 
 class Square:
-    """ Creates a square.
-    Private instance attributes: size
-    """
 
     def _init_(self, size):
-        """Initializes size"""
         self.__size = size
