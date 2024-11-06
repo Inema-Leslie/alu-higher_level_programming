acec3a83feb090450d3a95d0e6d776e5c50947b3
Inema_Leslie <a.inema2@alustudent.com>
2024-11-06 05:19:42 -0800

    n0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 class Rectangle:
-    pass 
+    """
+    A class that defines a rectangle.
+
+    This is an empty class that currently does not have any attributes or methods.
+    It is used to demonstrate the creation of a basic class.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
+
 
