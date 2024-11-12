cf5a7e28f1a8c4ab94d849ecc16940db4b752878
Inema_Leslie <a.inema2@alustudent.com>
2024-11-12 06:10:25 -0800

    n2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,2 +1,4 @@
--- A script that deletes the database.
-DROP DATABASE IF EXISTS hbtn_0c_0;
+-- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
