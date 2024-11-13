34458474a06c352b530c30d3bef1cec37b567013
Inema_Leslie <a.inema2@alustudent.com>
2024-11-12 21:23:22 -0800

    n10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
--- List all records of the table second table.
-SELECT score, name FROM second_table ORDER BY score DESC;
+-- Write a script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
