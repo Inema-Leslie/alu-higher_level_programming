5a92b367ec15f5c9b4e1625140ad29b167b1d676
Inema_Leslie <a.inema2@alustudent.com>
2024-11-12 21:37:49 -0800

    n15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,2 +1,6 @@
--- List the number of records with the same score.
-SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT tv_shows.title FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
