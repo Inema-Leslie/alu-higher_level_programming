6d85802802f0701384011740710d1b92dfc841a0
Inema_Leslie <a.inema2@alustudent.com>
2024-11-12 21:43:35 -0800

    n16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,5 @@
--- Script that lists all records of the table where name is not null.
-SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+-- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT tv_shows.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
